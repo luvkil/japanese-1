3b43a73be87134ff2e6319113a16476a3ee8573c
steven <stevennite1@gmail.com>
2021-06-01 08:28:37 -0400

    sixth commit

--- javaScript.js
+++ javaScript.js
@@ -4,16 +4,19 @@
 var startContent = document.querySelector('#start-content-hidden');
 var startButton = document.querySelector('.start-button');
 var quizTime = document.querySelector('#time'); 
-var runningScore = document.querySelector('#score')
+var runningScore = document.querySelector('#score');
+var finalScore = document.querySelector('#final-score');
+
+
 
 var questionContent = document.querySelector('#question-content');
-var quizQuestion = document.querySelector('#quiz--questions');
+var quizQuestion = document.querySelector('#quiz-questions');
 var choiceList = document.querySelector('.choicelist');
 
 var correctOrInncorrect = document.querySelector('#correct-incorrect');
 var Result = document.querySelector('#result');
 var formInitial = document.querySelector('initial');
-var highScores = document.querySelector('#high-scores-container-hidden');
+var highScores = document.querySelector('#high-scores-container');
 var submitButton = document.querySelector('#submit-btn');
 var highScoreList = document.querySelector('#high-score-list');
 var first = document.querySelector('#first'); 
@@ -25,6 +28,8 @@ var questionsAndChoices;
 var randomQuestion;
 var Time;
 var score;
+var initials;
+var outcomeTImer;
 
 
 
@@ -66,8 +71,10 @@ function questionSet (){
 };
 
 function main(){
- questionSet();
- nextQuestionsAndChoices();
+score = 0;
+runningScore.textContent = score;
+finalScore.textContent = score;
+ 
 
 //setting time interval
  var runningTime = 60;
@@ -80,7 +87,6 @@ function main(){
  if(Time === 0){
     alert("quiz is over");
     clearInterval(time);
-    //change to score card
     }
 
   }, 1000);
@@ -98,12 +104,12 @@ for( let i = 0; i < questionsAndChoices[randomQuestion].choices.length;i++ ){
     };
 
 //random quiz questions 
-//quizQuestion.textContent = questionsAndChoices.qestions
-for(let index = 0; index < questionsAndChoices[randomQuestion].questions.length;index++){
-        quizQuestion[index].textContent = questionsAndChoices[randomQuestion].questions[index];
+    //quizQuestion.textContent = questionsAndChoices[randomQuestion].qestions;
+    for(let j = 0; j < questionsAndChoices[randomQuestion].questions.length;j++){
+        quizQuestion.textContent = questionsAndChoices[randomQuestion].questions;
     }
 
-}
+};
 
 //function creating hidden section
 function change(element){
@@ -114,11 +120,80 @@ function change(element){
     element.display.style = "display-block";
 }
 
+//how do I create local storage??
 
 function intial(){
-    var currenScore = {};
+    var currentScore = {};
     var highScore = [];
+    
+    initials = formInitial;
+    currentScore.initials = initials;
+    highScore.score = score;
 
+    if(localStorage.getitem("highScore")){
+        highScore = JSON.parse(localStorage.getItem("highscore"))
+    }
 
+    highScore.push(currentScore);
+    console.log('------------------------------------');
+    console.log('highscore object =', highScore );
+    console.log('------------------------------------');
+
+    //set high score in local storage
+    localStorage.setitem("highScore", JSON.stringyfy(highScore));
+
+    //change to highscore
+    change(highScores);
+    //update high score
 
 }
+
+function gradeQestions(event) {
+    var countdown =1;
+//this code will run when function is triggered
+    var outcome = (questionsAndChoices[randomQuestion].anwers == event.target.textContent);
+// logic condition for checking answers and reseting time
+    if(outcome){
+        correctOrInncorrect.textContent = "true";
+        score++;
+        runningScore.textContent = score;
+        finalScore.textContent = score;
+    }else{
+        correctOrInncorrect.textContent= "false!";
+        runningTime -=2;
+    }
+
+
+    outcomeTImer = setInterval(() => {
+        if(countdown <= 0){
+            correctOrInncorrect.textContent = "";
+            clearInterval(outcomeTImer)
+        }
+    }, 1000);
+
+    //remove question from list
+    questionsAndChoices.splice(randomQuestion, 1);
+
+}
+
+/****************starting calls************* */
+//update high scores
+
+startButton.addEventListener("click", function(){
+questionSet();
+intial();
+nextQuestionsAndChoices();
+change(Result);
+change(questionContent);
+change(startContent);
+startContent.display.style = "none";
+});
+
+choiceList.addEventListener("click", function(){
+    correctOrInncorrect.textContent = "";
+    clearInterval(outcomeTImer);
+    gradeQestions();
+    nextQuestionsAndChoices();
+
+
+})
